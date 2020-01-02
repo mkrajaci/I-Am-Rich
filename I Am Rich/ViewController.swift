543a2056321b21c9b55881cc60cfa3fd0eef82e0
Mario Krajacic <krajacic.mario@gmail.com>
2020-01-02 19:27:01 +0100

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Mario Krajačić on 02/01/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
